8c962627e3b94675eeb78e2e7abb4643f8e94fe8
IgorRoc <igor_roc@hotmail.com.br>
2021-08-10 17:04:55 -0300

    [add]: Form working

--- js/reserva.js
+++ js/reserva.js
@@ -1,13 +1,21 @@
-function reservar() {
+var url = new URL(window.location)
+var nome = url.searchParams.get("nome");
+var hora = url.searchParams.get("nome");
+var data = url.searchParams.get("nome");
+
+function reservar(event) {
+	var form = this
+
 	var nome = document.getElementById("nome")
 	var hora = document.getElementById("hora")
 	var data = document.getElementById("data")
 
-    var parseData = new Date(data.value)
+	var parseData = new Date(data.value)
 
 	var pedido = `Oi! Meu nome é **${nome.value}** e gostaria de fazer uma reserva para o dia **${data.value}** às **${hora.value}**`
 
-	console.log(parseData)
-	console.log(data.value)
-	alert(pedido)
+    location.replace(`https://api.whatsapp.com/send?phone=557399837885&text=${pedido}`)
+
+	console.log(form.action)
+	event.preventDefault()
 }
