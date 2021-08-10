f666caec39464675c7e1c49b8401b4caa6ce6cde
IgorRoc <igor_roc@hotmail.com.br>
2021-08-10 17:19:32 -0300

    [fix]: Bold whatsapp text

--- js/reserva.js
+++ js/reserva.js
@@ -1,8 +1,3 @@
-var url = new URL(window.location)
-var nome = url.searchParams.get("nome");
-var hora = url.searchParams.get("nome");
-var data = url.searchParams.get("nome");
-
 function reservar(event) {
 	var form = this
 
@@ -12,7 +7,7 @@ function reservar(event) {
 
 	var parseData = new Date(data.value)
 
-	var pedido = `Oi! Meu nome é **${nome.value}** e gostaria de fazer uma reserva para o dia **${data.value}** às **${hora.value}**`
+	var pedido = `Oi! Meu nome é *${nome.value}* e gostaria de fazer uma reserva para o dia *${data.value}* às *${hora.value}*`
 
     location.replace(`https://api.whatsapp.com/send?phone=557399837885&text=${pedido}`)
 
